edfc306b9300563d291d543af9fe269d1017969b
jpagotaisidro <32203421+jpagotaisidro@users.noreply.github.com>
2022-07-19 11:46:40 -0400

    Add files via upload

---
+++ User.java
@@ -0,0 +1,26 @@
+package com.example.ngoDonation.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="USER")
+public class User {
+	private String firstName;
+	private String lastName;
+	private String email;
+	private boolean admin; //true is admin
+}
