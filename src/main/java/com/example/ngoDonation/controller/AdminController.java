a09a9ad632d7903720b307bfefac044f290892ca
jpagotaisidro <32203421+jpagotaisidro@users.noreply.github.com>
2022-07-20 10:50:11 -0400

    Added data to database,
    working on setting up display users, updated database with some data

--- src/main/java/com/example/ngoDonation/controller/AdminController.java
+++ src/main/java/com/example/ngoDonation/controller/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.ngoDonation.entity.User;
 import com.example.ngoDonation.service.AdminService;
@@ -40,4 +41,11 @@ public class AdminController {
 		public String editUser() {
 			return "edituser";
 		}
+		
+//		@RequestMapping("/users")
+//	    public ModelAndView getUsers() {
+//	        ModelAndView modelAndView = new ModelAndView();
+//	        modelAndView.setViewName("users.html");
+//	        return modelAndView;
+//	    }  
 }
